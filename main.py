b7d9688d77993c60a14fcc0d68c0bc98cdfc0a2e
nyanmp3 <bengit2010@gmail.com>
2025-12-06 14:11:40 +0300

    Create class MainWindow and setup size, title, fullscreen.

--- main.py
+++ main.py
@@ -1,1 +1,25 @@
-print("Hello CowBoy")
+import arcade
+
+width_user, height_user = arcade.get_display_size()
+
+
+class MainWindow(arcade.Window):
+    def __init__(self, width, height, title):
+        super().__init__(width, height, title, resizable=False, fullscreen=True)
+        self.background_color = arcade.color.TEA_GREEN
+
+    def setup(self):
+        pass
+
+    def on_draw(self):
+        self.clear()
+
+
+def main():
+    game = MainWindow(width_user, height_user, "Deadly_Hunt")
+    game.setup()
+    arcade.run()
+
+
+if __name__ == "__main__":
+    main()
