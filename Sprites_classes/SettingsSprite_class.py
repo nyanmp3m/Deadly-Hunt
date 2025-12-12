e187ab83d0d3b2e15581689d0a7e810e9acf8b44
nyanmp3 <bengit2010@gmail.com>
2025-12-12 14:27:14 +0300

    Add first sprite on screen

--- Sprites_classes/SettingsSprite_class.py
+++ Sprites_classes/SettingsSprite_class.py
@@ -1,5 +1,17 @@
 import arcade
 
+width_user, height_user = arcade.get_display_size()
 
 class SettingsSprite(arcade.Sprite):
-    pass
+    def __init__(self):
+        super().__init__()
+
+        self.scale = 0.25
+        self.speed = 0
+        self.health = 1
+
+        self.idle_texture = arcade.load_texture("Pictures/SettingsSprite_picture.png")
+        self.texture = self.idle_texture
+
+        self.center_x = width_user // 2
+        self.center_y = height_user // 2
--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+from pyglet.event import EVENT_HANDLE_STATE
+
+from Sprites_classes.SettingsSprite_class import SettingsSprite
 import arcade
 
 width_user, height_user = arcade.get_display_size()
@@ -5,14 +8,24 @@ width_user, height_user = arcade.get_display_size()
 
 class MainWindow(arcade.Window):
     def __init__(self, width, height, title):
-        super().__init__(width, height, title, resizable=False, fullscreen=True)
+        super().__init__(width, height, title, resizable=False, fullscreen=False)
         self.background_color = arcade.color.TEA_GREEN
+        self.sprite_list = arcade.SpriteList()
 
     def setup(self):
         pass
 
     def on_draw(self):
+        setting = SettingsSprite()
+        self.sprite_list.append(setting)
+
         self.clear()
+        self.sprite_list.draw()
+
+
+    def on_mouse_press(self, x, y, button, modifiers):
+        setting_hit = arcade.get_sprites_at_point((x, y), self.sprite_list)
+
 
 
 def main():
