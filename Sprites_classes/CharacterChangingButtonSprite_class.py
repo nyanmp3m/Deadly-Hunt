6053470497f1c6d7a449be0d62cab114ddb8b6f6
Retr0 <kos.kokorev@gmail.com>
2025-12-21 21:47:46 +0300

    Макеты еще 2-х кнопок. Не трогайте, я жду пока Даня сделает картинки и дальше уже буду регулировать местоположение

---
+++ Sprites_classes/CharacterChangingButtonSprite_class.py
@@ -0,0 +1,12 @@
+import arcade
+
+width_user, height_user = arcade.get_display_size()
+
+class StartGameButtonSprite(arcade.Sprite):
+    def __init__(self):
+        super().__init__()
+        self.scale = 0.8
+
+        self.texture = arcade.load_texture("")
+        self.center_x = (width_user // 2) - (width_user // 2.5)
+        self.center_y = height_user // 2
