292381602790ebd822c3fca98c03ce47a7d1a7fa
Kitsune13th <kos.kokorev@gmail.com>
2026-01-09 12:29:36 +0300

    Сделан выбор персонажа. Осталось добавить других и сделать все красивее

--- StartingWindow/StartingWindow.py
+++ StartingWindow/StartingWindow.py
@@ -163,6 +163,10 @@ class CharacterChangeView(arcade.View):
         self.window = window
         self.fullscreen = False
         arcade.set_background_color(self.background)
+        self.cursor_list = arcade.SpriteList()
+
+        self.cursor = Cursor(width_user // 2, height_user // 2)
+        self.cursor_list.append(self.cursor)
 
         self.characters_choice_list = arcade.SpriteList()
         self.current_character_index = 0
@@ -170,6 +174,7 @@ class CharacterChangeView(arcade.View):
 
         self.knight_character = KnightCharacterSprite()
         self.characters_choice_list.append(self.knight_character)
+        self.knight_character.visible = True
 
         self.gnome_character = GnomeCharacterSprite()
         self.characters_choice_list.append(self.gnome_character)
@@ -189,9 +194,31 @@ class CharacterChangeView(arcade.View):
         self.characters_choice_list.draw()
         self.arrow_left_list.draw()
         self.arrow_right_list.draw()
+        self.cursor_list.draw()
 
     def on_update(self, delta_time):
         pass
+
+    def on_mouse_motion(self, x, y, dx, dy):
+        self.cursor_list[0].center_x = x
+        self.cursor_list[0].center_y = y
+
+    def on_mouse_release(self, x, y, button, modifiers):
+        if button == arcade.MOUSE_BUTTON_LEFT:
+            if arcade.get_sprites_at_point((x, y), self.arrow_right_list):
+                self.characters_choice_list[self.current_character_index].visible = False
+                self.current_character_index += 1
+                if self.current_character_index >= len(self.characters_choice_list):
+                    self.current_character_index = 0
+                    self.characters_choice_list[self.current_character_index].visible = True
+                self.characters_choice_list[self.current_character_index].visible = True
+            elif arcade.get_sprites_at_point((x, y), self.arrow_left_list):
+                self.characters_choice_list[self.current_character_index].visible = False
+                self.current_character_index -= 1
+                if self.current_character_index < 0:
+                    self.current_character_index = len(self.characters_choice_list) - 1
+                    self.characters_choice_list[self.current_character_index].visible = True
+                self.characters_choice_list[self.current_character_index].visible = True
 
 
 
