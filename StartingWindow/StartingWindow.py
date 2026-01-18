179db40df4b7cdb1461a4532ae1c1b18605f1340
Kitsune13th <kos.kokorev@gmail.com>
2026-01-18 18:15:02 +0300

    ИдИ нАхРеН

--- StartingWindow/StartingWindow.py
+++ StartingWindow/StartingWindow.py
@@ -1,21 +1,11 @@
 import arcade
 import random
 
-from Sprites_classes.ElfCharacterSprite_class import ElfCharacterSprite
 from Sprites_classes.SettingsSprite_class import SettingsSprite
 from Sprites_classes.SettingsSpriteWindow_class import SettingsWindow
 from Sprites_classes.StartGameButtonSprite_class import StartGameButtonSprite
 from Sprites_classes.explosion import Explosion
 from Sprites_classes.skeleton import Skeleton
-from Sprites_classes.CharacterChangingButtonSprite_class import CharacterChangeButtonSprite
-from Sprites_classes.KnightCharacterSprite_class import KnightCharacterSprite
-from Sprites_classes.GnomeCharacterSprite_class import GnomeCharacterSprite
-from Sprites_classes.SamuraiCharacterSprite_class import SamuraiCharacterSprite
-from Sprites_classes.GladiatorCharacterSprite_class import GladiatorCharacterSprite
-from Sprites_classes.ElfCharacterSprite_class import ElfCharacterSprite
-from Sprites_classes.ChangeCharacterArrowLeftSprite_class import ChangeCharacterArrowLeftSprite
-from Sprites_classes.ChangeCharacterArrowRightSprite_class import ChangeCharacterArrowRightSprite
-from Sprites_classes.ApproveCharacterButtonSprite_class import ApproveCharacterButtonSprite
 from Sprites_classes.Cursor_texture import Cursor
 
 
@@ -34,7 +24,6 @@ class MainMenu(arcade.View):
         self.explosion_animation_list = arcade.SpriteList()
         self.cursor_list = arcade.SpriteList()
         self.skeleton_list = arcade.SpriteList()
-        self.change_character_button_list = arcade.SpriteList()
         self.window = window
 
         self.explosion_flag = False
@@ -44,9 +33,6 @@ class MainMenu(arcade.View):
 
         self.start_game_button = StartGameButtonSprite()
         self.start_game_list.append(self.start_game_button)
-
-        self.change_character_button = CharacterChangeButtonSprite()
-        self.change_character_button_list.append(self.change_character_button)
 
         setting = SettingsSprite()
         self.settings_sprite_list.append(setting)
@@ -69,7 +55,6 @@ class MainMenu(arcade.View):
                                  arcade.rect.XYWH(self.width // 2, self.height // 2, self.width, self.height))
         self.settings_sprite_list.draw()
         self.start_game_list.draw()
-        self.change_character_button_list.draw()
         if background == "background1.jpg":
             self.skeleton_list.draw()
         self.explosion_animation_list.draw()
@@ -125,12 +110,9 @@ class MainMenu(arcade.View):
                 sprite.hp -= 1
 
         if self.start_game_button.collides_with_point((x, y)):
-            game_view = GameView(self.window)
-            self.window.show_view(game_view)
+            new_game_menu = NewGameWindowView(self.window)
+            self.window.show_view(new_game_menu)
 
-        if self.change_character_button.collides_with_point((x, y)):
-            character_change_view = CharacterChangeView(self.window)
-            self.window.show_view(character_change_view)
 
     def on_key_press(self, key, modifier):
         if key == arcade.key.RCTRL:
@@ -142,195 +124,19 @@ class MainMenu(arcade.View):
                 self.fullscreen_flag = True
 
 
-class GameView(arcade.View):
+class NewGameWindowView(arcade.View):
     def __init__(self, window):
         super().__init__(window)
-        self.background = arcade.color.DARK_GREEN
         self.window = window
-        self.fullscreen = False
-        arcade.set_background_color(self.background)
-        self.window.set_mouse_visible(False)
-
-        self.explosion_animation_list = arcade.SpriteList()
-        self.explosion_flag = False
-
+        arcade.set_background_color(arcade.color.GRAY)
         self.cursor_list = arcade.SpriteList()
 
         self.cursor = Cursor(width_user // 2, height_user // 2)
         self.cursor_list.append(self.cursor)
 
-        self.flag_mouse_on_start_button = False
-        self.fullscreen_flag = True
-
     def on_draw(self):
         self.clear()
-        self.explosion_animation_list.draw()
         self.cursor_list.draw()
-
-    def on_update(self, delta_time):
-        finished_animation = []
-        for i in self.explosion_animation_list:
-            i.update_animation(delta_time)
-            if i.finished:
-                finished_animation.append(i)
-
-        for i in finished_animation:
-            self.explosion_animation_list.remove(i)
-
-    def on_mouse_motion(self, x, y, dx, dy):
-        self.cursor_list[0].center_x = x
-        self.cursor_list[0].center_y = y
-
-    def on_mouse_press(self, x, y, button, modifiers):
-        if len(self.explosion_animation_list) == 0:
-            explosion = Explosion(x, y + 20)
-            self.explosion_animation_list.append(explosion)
-
-    def on_key_press(self, key, modifiers):
-        if key == arcade.key.RCTRL:
-            if not self.fullscreen:
-                self.window.set_fullscreen(False)
-                self.fullscreen = True
-            elif self.fullscreen:
-                self.window.set_fullscreen(True)
-                self.fullscreen = False
-
-        if key == arcade.key.ESCAPE:
-            main_menu = MainMenu(self.window)
-            self.window.show_view(main_menu)
-
-
-
-
-
-class CharacterChangeView(arcade.View):
-    def __init__(self, window):
-        super().__init__(window)
-        self.background = arcade.color.DARK_GREEN
-        self.window = window
-        self.fullscreen = False
-        self.background = arcade.load_texture("Pictures/Character_changing_menu_background.png")
-        self.window.set_mouse_visible(False)
-        self.cursor_list = arcade.SpriteList()
-        self.explosion_animation_list = arcade.SpriteList()
-
-        self.explosion_flag = False
-
-        self.cursor = Cursor(width_user // 2, height_user // 2)
-        self.cursor_list.append(self.cursor)
-
-        self.characters_choice_list = arcade.SpriteList()
-        self.current_character_index = 0
-        self.characters_names = ["Рыцарь", "Гном", "Самурай", "Гладиатор", "Эльф"]
-
-        self.knight_character = KnightCharacterSprite()
-        self.characters_choice_list.append(self.knight_character)
-        self.knight_character.visible = True
-
-        self.gnome_character = GnomeCharacterSprite()
-        self.characters_choice_list.append(self.gnome_character)
-        self.gnome_character.visible = False
-
-        self.samurai_character = SamuraiCharacterSprite()
-        self.characters_choice_list.append(self.samurai_character)
-        self.samurai_character.visible = False
-
-        self.gladiator_character = GladiatorCharacterSprite()
-        self.characters_choice_list.append(self.gladiator_character)
-        self.gladiator_character.visible = False
-
-        self.elf_character = ElfCharacterSprite()
-        self.characters_choice_list.append(self.elf_character)
-        self.elf_character.visible = False
-
-        self.arrow_left_list = arcade.SpriteList()
-        self.arrow_left = ChangeCharacterArrowLeftSprite()
-        self.arrow_left_list.append(self.arrow_left)
-
-        self.arrow_right_list = arcade.SpriteList()
-        self.arrow_right = ChangeCharacterArrowRightSprite()
-        self.arrow_right_list.append(self.arrow_right)
-
-        self.approve_character_button_list = arcade.SpriteList()
-        self.approve_character_button = ApproveCharacterButtonSprite()
-        self.approve_character_button_list.append(self.approve_character_button)
-
-        self.fullscreen_flag = True
-
-        self.flag_mouse_on_start_button = False
-
-    def on_draw(self):
-        self.clear()
-        arcade.draw_texture_rect(self.background,
-                                 arcade.rect.XYWH(self.width // 2, self.height // 2, self.width, self.height))
-        arcade.draw_text(self.characters_names[self.current_character_index], self.window.width // 2,
-                                   self.window.height * 0.9, arcade.color.WHITE, 40, anchor_x="center", anchor_y="center")
-        self.characters_choice_list.draw()
-        self.arrow_left_list.draw()
-        self.arrow_right_list.draw()
-        self.approve_character_button_list.draw()
-        self.explosion_animation_list.draw()
-        self.cursor_list.draw()
-
-    def on_update(self, delta_time):
-
-        finished_animation = []
-        for i in self.explosion_animation_list:
-            i.update_animation(delta_time)
-            if i.finished:
-                finished_animation.append(i)
-
-        for i in finished_animation:
-            self.explosion_animation_list.remove(i)
-
-    def on_mouse_motion(self, x, y, dx, dy):
-        self.cursor_list[0].center_x = x
-        self.cursor_list[0].center_y = y
-
-    def on_mouse_press(self, x, y, button, modifiers):
-        if len(self.explosion_animation_list) == 0:
-            explosion = Explosion(x, y + 20)
-            self.explosion_animation_list.append(explosion)
-
-        if button == arcade.MOUSE_BUTTON_LEFT:
-            if arcade.get_sprites_at_point((x, y), self.arrow_right_list):
-                self.characters_choice_list[self.current_character_index].visible = False
-                self.current_character_index += 1
-                if self.current_character_index >= len(self.characters_choice_list):
-                    self.current_character_index = 0
-                    self.characters_choice_list[self.current_character_index].visible = True
-                self.characters_choice_list[self.current_character_index].visible = True
-            elif arcade.get_sprites_at_point((x, y), self.arrow_left_list):
-                self.characters_choice_list[self.current_character_index].visible = False
-                self.current_character_index -= 1
-                if self.current_character_index < 0:
-                    self.current_character_index = len(self.characters_choice_list) - 1
-                    self.characters_choice_list[self.current_character_index].visible = True
-                self.characters_choice_list[self.current_character_index].visible = True
-
-            if self.approve_character_button.collides_with_point((x, y)):
-                main_menu = MainMenu(self.window)
-                self.window.show_view(main_menu)
-
-    def on_key_press(self, key, modifiers):
-        if key == arcade.key.ESCAPE:
-            main_menu = MainMenu(self.window)
-            self.window.show_view(main_menu)
-
-        if key == arcade.key.RIGHT or key == arcade.key.D:
-            self.characters_choice_list[self.current_character_index].visible = False
-            self.current_character_index += 1
-            if self.current_character_index >= len(self.characters_choice_list):
-                self.current_character_index = 0
-                self.characters_choice_list[self.current_character_index].visible = True
-            self.characters_choice_list[self.current_character_index].visible = True
-        elif key == arcade.key.LEFT or key == arcade.key.A:
-            self.characters_choice_list[self.current_character_index].visible = False
-            self.current_character_index -= 1
-            if self.current_character_index < 0:
-                self.current_character_index = len(self.characters_choice_list) - 1
-                self.characters_choice_list[self.current_character_index].visible = True
-            self.characters_choice_list[self.current_character_index].visible = True
 
 
 
